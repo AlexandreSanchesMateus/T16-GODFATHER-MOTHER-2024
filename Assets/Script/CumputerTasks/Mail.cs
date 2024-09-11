9e7a45c490709148acc96886a3e059a2df43ed0e
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2024-09-11 14:18:15 +0200

    Mail v1 finished

--- Assets/Script/CumputerTasks/Mail.cs
+++ Assets/Script/CumputerTasks/Mail.cs
@@ -7,9 +7,13 @@ public class Mail : MonoBehaviour
 {
     [SerializeField]
     private List<string> destinationNames = new List<string>();
+    [SerializeField]
+    private List<string> subjectNames = new List<string>();
 
     [SerializeField]
     private TextMeshProUGUI destinataireTxt;
+    [SerializeField]
+    private TextMeshProUGUI subjectTxt;
     [SerializeField]
     private TextMeshProUGUI corpTxt;
     [SerializeField]
@@ -20,7 +24,10 @@ public class Mail : MonoBehaviour
     public void InitMail(int nbLettersRequested)
     {
         if (destinationNames.Count > 0)
-            destinataireTxt.text = "A " + destinationNames[Random.Range(0, destinationNames.Count)];
+            destinataireTxt.text = destinationNames[Random.Range(0, destinationNames.Count)];
+
+        if (subjectNames.Count > 0)
+            subjectTxt.text = subjectNames[Random.Range(0, subjectNames.Count)];
 
         targetLetterNb = nbLettersRequested;
         nbLetterTxt.text = "0/ " + targetLetterNb;
@@ -29,6 +36,12 @@ public class Mail : MonoBehaviour
     public void White(string str)
     {
         corpTxt.text += str;
+        nbLetterTxt.text = corpTxt.text.Length + "/ " + targetLetterNb;
+    }
+
+    public void BackSpace() 
+    {
+        corpTxt.text = corpTxt.text.Remove(corpTxt.text.Length - 1);
         nbLetterTxt.text = corpTxt.text.Length + "/ " + targetLetterNb;
     }
 
--- Assets/Script/CumputerTasks/MailTask.cs
+++ Assets/Script/CumputerTasks/MailTask.cs
@@ -1,24 +1,45 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MailTask : MonoBehaviour
 {
-    private List<Mail> _mails;
+    private Queue<Mail> _mails;
     private Mail _currentMail;
 
-    [SerializeField]
+    bool _canRemove = true;
+
+    [SerializeField, BoxGroup("Init")]
     private GameObject mailPrefab;
+    [SerializeField, BoxGroup("Init")]
+    private GameObject mailNotification;
+    [SerializeField, BoxGroup("Init")]
+    private TextMeshProUGUI notifTxt;
 
-    [SerializeField]
+    [SerializeField, BoxGroup("Settings")]
     private float backspaceFrk = 0.2f;
-    [SerializeField]
-    private int MaxStackableMail = 3;
+    [SerializeField, BoxGroup("Settings")]
+    private int MaxStackableMail = 5;
 
-    [SerializeField]
+    [SerializeField, BoxGroup("Settings")]
     private int minLetterRequested = 20;
-    [SerializeField]
+    [SerializeField, BoxGroup("Settings")]
     private int maxLetterRequested = 120;
+
+    [SerializeField, Foldout("Events")]
+    private UnityEvent OnMailArrive;
+    [SerializeField, Foldout("Events")]
+    private UnityEvent OnMailSendGood;
+    [SerializeField, Foldout("Events")]
+    private UnityEvent OnMailSendBad;
+    [SerializeField, Foldout("Events")]
+    private UnityEvent OnWhiteMail;
+    [SerializeField, Foldout("Events")]
+    private UnityEvent OnRevertMail;
+
 
     public void CreateNewMail()
     {
@@ -26,29 +47,67 @@ public class MailTask : MonoBehaviour
         if (_mails.Count >= MaxStackableMail)
             return;
 
-        GameObject mail = Instantiate(mailPrefab,new Vector3(0, 0, 0), Quaternion.identity, transform);
+        Mail mail = Instantiate(mailPrefab,new Vector3(0, 0, 0), Quaternion.identity, transform).GetComponent<Mail>();
+        mail.InitMail(Random.Range(minLetterRequested, maxLetterRequested));
         mail.transform.SetAsFirstSibling();
 
         // Ajouter à la liste
-        _mails.Add(mail.GetComponent<Mail>());
-    }
+        _mails.Enqueue(mail);
 
+        // Notif
+        mailNotification.SetActive(true);
+        notifTxt.text = _mails.Count.ToString();
+
+        OnMailArrive?.Invoke();
+    }
+    
     public void SendNewKeystrok(string str)
     {
         // Ecrire dans le TextMeshPro
         _currentMail.White(str);
+        OnWhiteMail?.Invoke();
     }
 
     public void RemoveLastKeystrok()
     {
         // Effacer avec une fréquence
+        if (_canRemove)
+        {
+            _currentMail.BackSpace();
+            OnRevertMail?.Invoke();
 
-        // Actualiser le nombre de lettre
+            _canRemove = false;
+            StartCoroutine("WaitToRemove");
+        }
     }
 
     public void SendMail()
     {
         // Envoyer le mail
         // Vérifier taille !
+
+        if (_mails.Dequeue().Send())
+            OnMailSendGood?.Invoke();
+        else
+            OnMailSendBad?.Invoke();
+
+        if(_mails.Count > 0)
+
+        if (_mails.Count <= 0)
+        {
+            mailNotification.SetActive(false);
+            _currentMail = null;
+        }
+        else
+        {
+            _currentMail = _mails.Peek();
+            notifTxt.text = _mails.Count.ToString();
+        }
+    }
+
+    IEnumerable WaitToRemove()
+    {
+        yield return new WaitForSeconds(backspaceFrk);
+        _canRemove = true;
     }
 }

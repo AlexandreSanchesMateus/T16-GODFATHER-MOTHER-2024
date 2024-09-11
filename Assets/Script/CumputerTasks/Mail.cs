0ea81f844b6201d3efd73b33b967fbf47d9a3ff4
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2024-09-11 12:15:51 +0200

    Mail & popup in progress

--- Assets/Script/CumputerTasks/Mail.cs
+++ Assets/Script/CumputerTasks/Mail.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Mail : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private List<string> destinationNames = new List<string>();
+
+    [SerializeField]
+    private TextMeshProUGUI destinataireTxt;
+    [SerializeField]
+    private TextMeshProUGUI corpTxt;
+    [SerializeField]
+    private TextMeshProUGUI nbLetterTxt;
+
+    private int targetLetterNb;
+
+    public void InitMail(int nbLettersRequested)
     {
-        
+        if (destinationNames.Count > 0)
+            destinataireTxt.text = "A " + destinationNames[Random.Range(0, destinationNames.Count)];
+
+        targetLetterNb = nbLettersRequested;
+        nbLetterTxt.text = "0/ " + targetLetterNb;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void White(string str)
     {
-        
+        corpTxt.text += str;
+        nbLetterTxt.text = corpTxt.text.Length + "/ " + targetLetterNb;
+    }
+
+    public bool Send()
+    {
+        Destroy(gameObject);
+        return corpTxt.text.Length == targetLetterNb;
     }
 }
--- Assets/Script/CumputerTasks/MailTask.cs
+++ Assets/Script/CumputerTasks/MailTask.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class MailTask : MonoBehaviour
 {
-    /*private List<Mail> _mails;
-    private Mail _currentMail;*/
+    private List<Mail> _mails;
+    private Mail _currentMail;
+
+    [SerializeField]
+    private GameObject mailPrefab;
 
     [SerializeField]
     private float backspaceFrk = 0.2f;
     [SerializeField]
     private int MaxStackableMail = 3;
 
+    [SerializeField]
+    private int minLetterRequested = 20;
+    [SerializeField]
+    private int maxLetterRequested = 120;
+
     public void CreateNewMail()
     {
         // Faire un mail
+        if (_mails.Count >= MaxStackableMail)
+            return;
+
+        GameObject mail = Instantiate(mailPrefab,new Vector3(0, 0, 0), Quaternion.identity, transform);
+        mail.transform.SetAsFirstSibling();
+
         // Ajouter à la liste
+        _mails.Add(mail.GetComponent<Mail>());
     }
 
     public void SendNewKeystrok(string str)
     {
         // Ecrire dans le TextMeshPro
-
-        // Actualiser le nombre de lettre
+        _currentMail.White(str);
     }
 
     public void RemoveLastKeystrok()

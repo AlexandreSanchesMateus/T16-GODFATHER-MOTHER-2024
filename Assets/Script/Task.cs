ae03de67465f4ffde47c2f435f51e249a909978d
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2024-09-12 13:59:02 +0200

    Add visuel and tweeking

--- Assets/Script/Task.cs
+++ Assets/Script/Task.cs
@@ -6,19 +6,24 @@ using UnityEngine.Events;
 
 public abstract class Task : MonoBehaviour
 {
+    public bool ActiveOnStart = false;
+
     [SerializeField]
     private CamRotationManager.ECamRotState taskLocation;
     public CamRotationManager.ECamRotState CamRotState { get {  return taskLocation; } }
 
     [SerializeField, Foldout("Events")] protected UnityEvent<Task> _onTaskRecived;
     public event UnityAction<Task> onTaskRecived { add => _onTaskRecived.AddListener(value); remove => _onTaskRecived.RemoveListener(value); }
-
     [SerializeField, Foldout("Events")] protected UnityEvent<Task> _onTaskFinished;
     public event UnityAction<Task> onTaskFinished { add => _onTaskFinished.AddListener(value); remove => _onTaskFinished.RemoveListener(value); }
 
     public bool HaveTask { get; protected set; }
-
     public bool IsActive { get; private set; } = false;
+
+    private void Awake()
+    {
+        IsActive = ActiveOnStart;
+    }
 
     public virtual void Activate() { IsActive = true; }
     public virtual void Deactivate() { IsActive = false; }
